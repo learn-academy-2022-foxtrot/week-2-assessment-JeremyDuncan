29e36a1c031063fd015a3013612c616df3451423
JeremyDuncan <jeremy.duncan1984@gmail.com>
2022-09-16 13:41:35 -0700

    created branch

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,9 +14,9 @@
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
+const numbersArray1 = [6, 7, 8, 9, 10];
 // Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+const numbersArray2 = [24, 27, 30, 33, 36];
 // Expected output: [72, 81, 90, 99, 108]
 
 // b) Create the function that makes the test pass.
@@ -25,11 +25,11 @@ const numbersArray2 = [24, 27, 30, 33, 36]
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
+const object1 = { number: 15 };
 // Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
+const object2 = { number: 0 };
 // Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
+const object3 = { number: -7 };
 // Expected output: "-7 is not divisible by three"
 
 // b) Create the function that makes the test pass.
@@ -38,9 +38,9 @@ const object3 = { number: -7 }
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"];
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+const randomNouns2 = ["temperature", "database", "chopsticks", "mango"];
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
 // b) Create the function that makes the test pass.
@@ -49,11 +49,11 @@ const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const vowelTester1 = "learn"
+const vowelTester1 = "learn";
 // Expected output: 1
-const vowelTester2 = "academy"
+const vowelTester2 = "academy";
 // Expected output: 0
-const vowelTester3 = "challenges"
+const vowelTester3 = "challenges";
 // Expected output: 2
 
 // b) Create the function that makes the test pass.
